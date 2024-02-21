8451f2dc331ef4abaa75091f75e16e642f30f3e6
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-02-21 14:24:01 -0500

    board fully displayed

--- client/app.jsx
+++ client/app.jsx
@@ -14,10 +14,37 @@ const App = () => {
 };
 
 const Board = () => {
+  const grid = [];
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  let color;
+  for (let num = 8; num > 0; num--) {
+    const row = [];
+    if (num % 2 === 0) color = 'white';
+    else color = 'gray';
+    for (let lett = 0; lett < 8; lett++) {
+      row.push(<Square id={letters[lett] + num} color={color} />);
+      if (color === 'white') color = 'gray';
+      else color = 'white';
+    }
+    grid.push(<div>{row}</div>);
+  }
+  return grid;
+};
 
-  
-
-  return <text>board</text>;
+const Square = (props) => {
+  return (
+    <button
+      style={{
+        height: '70px',
+        width: '70px',
+        border: 'solid',
+        fontSize: '30px',
+        backgroundColor: props.color,
+      }}
+      id={props.id}
+      key={props.id}
+    ></button>
+  );
 };
 
 export default App;
