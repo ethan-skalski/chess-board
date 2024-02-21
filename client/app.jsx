e5f444f8c5db14d90d79420aad0db55c315ba837
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-02-21 15:00:35 -0500

    pieces in starting position

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
+const startingPieces = {
+  a1: ['R', 'w'],
+  b1: ['N', 'w'],
+  c1: ['B', 'w'],
+  d1: ['Q', 'w'],
+  e1: ['K', 'w'],
+  f1: ['B', 'w'],
+  g1: ['N', 'w'],
+  h1: ['R', 'w'],
+  a2: ['P', 'w'],
+  b2: ['P', 'w'],
+  c2: ['P', 'w'],
+  d2: ['P', 'w'],
+  e2: ['P', 'w'],
+  f2: ['P', 'w'],
+  g2: ['P', 'w'],
+  h2: ['P', 'w'],
+  a8: ['R', 'b'],
+  b8: ['N', 'b'],
+  c8: ['B', 'b'],
+  d8: ['Q', 'b'],
+  e8: ['K', 'b'],
+  f8: ['B', 'b'],
+  g8: ['N', 'b'],
+  h8: ['R', 'b'],
+  a7: ['P', 'b'],
+  b7: ['P', 'b'],
+  c7: ['P', 'b'],
+  d7: ['P', 'b'],
+  e7: ['P', 'b'],
+  f7: ['P', 'b'],
+  g7: ['P', 'b'],
+  h7: ['P', 'b'],
+};
+
 const App = () => {
   return (
     <>
@@ -16,15 +51,26 @@ const App = () => {
 const Board = () => {
   const grid = [];
   const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-  let color;
+  let sqColor;
   for (let num = 8; num > 0; num--) {
     const row = [];
-    if (num % 2 === 0) color = 'white';
-    else color = 'gray';
+    if (num % 2 === 0) sqColor = 'gainsboro';
+    else sqColor = 'gray';
     for (let lett = 0; lett < 8; lett++) {
-      row.push(<Square id={letters[lett] + num} color={color} />);
-      if (color === 'white') color = 'gray';
-      else color = 'white';
+      const coords = letters[lett] + num;
+      let piece = '',
+        pColor = 'black';
+      if (startingPieces[coords]) {
+        piece = startingPieces[coords][0];
+        if (startingPieces[coords][1] === 'w') {
+          pColor = 'white';
+        }
+      }
+      row.push(
+        <Square id={coords} sqColor={sqColor} piece={piece} pColor={pColor} />
+      );
+      if (sqColor === 'gainsboro') sqColor = 'gray';
+      else sqColor = 'gainsboro';
     }
     grid.push(<div>{row}</div>);
   }
@@ -38,12 +84,16 @@ const Square = (props) => {
         height: '70px',
         width: '70px',
         border: 'solid',
+        borderColor: 'black',
         fontSize: '30px',
-        backgroundColor: props.color,
+        color: props.pColor,
+        backgroundColor: props.sqColor,
       }}
       id={props.id}
       key={props.id}
-    ></button>
+    >
+      {props.piece}
+    </button>
   );
 };
 
