7689b948867ad28709dac491022ca8e27e28f086
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-02-21 17:40:59 -0500

    moving pieces

--- client/app.jsx
+++ client/app.jsx
@@ -83,11 +83,13 @@ const matrix = {
   },
 };
 
+let focus = '';
+
 const App = () => {
   return (
     <>
       <div>
-        <h1>Solo Project: A Pathetic Attempt at Chess</h1>
+        <h1>Solo Project: An Attempt at Chess</h1>
       </div>
       <div>
         <Board />
@@ -99,6 +101,22 @@ const App = () => {
 const Board = () => {
   const [boardState, setboardState] = useState(matrix);
 
+  const handleClick = (target) => {
+    console.log(target.id);
+    if (boardState[target.id[0]][target.id[1]][0] === '' && focus === '') {
+      focus = '';
+      console.log('BLANK');
+    } else if (focus === '') {
+      focus = target;
+    } else {
+      const newBoardState = JSON.parse(JSON.stringify(boardState));
+      newBoardState[focus.id[0]][focus.id[1]] = ['', ''];
+      newBoardState[target.id[0]][target.id[1]] =
+        boardState[focus.id[0]][focus.id[1]];
+      focus = '';
+      setboardState(newBoardState);
+    }
+  };
   const grid = [];
   const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
   let sqColor;
@@ -109,15 +127,21 @@ const Board = () => {
     for (let lett = 0; lett < 8; lett++) {
       const coords = letters[lett] + num;
       let piece = '',
-        pColor = 'black';
-      if (matrix[coords[0]][coords[1]]) {
-        piece = matrix[coords[0]][coords[1]][0];
-        if (matrix[coords[0]][coords[1]][1] === 'w') {
-          pColor = 'white';
-        }
+        pColor;
+      if (boardState[coords[0]][coords[1]]) {
+        piece = boardState[coords[0]][coords[1]][0];
+        if (boardState[coords[0]][coords[1]][1] === 'w') pColor = 'white';
+        else if (boardState[coords[0]][coords[1]][1] === 'b') pColor = 'black';
+        else pColor = '';
       }
       row.push(
-        <Square id={coords} sqColor={sqColor} piece={piece} pColor={pColor} />
+        <Square
+          id={coords}
+          sqColor={sqColor}
+          piece={piece}
+          pColor={pColor}
+          handleClick={handleClick}
+        />
       );
       if (sqColor === 'gainsboro') sqColor = 'gray';
       else sqColor = 'gainsboro';
@@ -139,6 +163,7 @@ const Square = (props) => {
         color: props.pColor,
         backgroundColor: props.sqColor,
       }}
+      onClick={(e) => props.handleClick(e.target)}
       id={props.id}
       key={props.id}
     >
